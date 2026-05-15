b0120f453577c91ebafb5f43c99dc1ca33505431
kaiST <phucnguyen17072005@gmail.com>
2026-05-15 09:54:23 +0700

    first commit

---
+++ src/main/java/com/example/KaiST/sgu_admission_system/SguAdmissionSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.KaiST.sgu_admission_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SguAdmissionSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(SguAdmissionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KaiST/sgu_admission_system/SguAdmissionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KaiST.sgu_admission_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SguAdmissionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
